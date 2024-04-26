a2e5cf93082c1040325159edb35dab34df23cb6b
Chris Sewell <chrisj_sewell@hotmail.com>
2024-04-26 23:03:15 +0200

    Add intersphinx refs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,13 @@ from sphinx_rust import __version__
 project = "sphinx-rust"
 version = __version__
 
-extensions = ["sphinx_rust", "myst_parser", "sphinx_needs", "sphinxcontrib.plantuml"]
+extensions = [
+    "sphinx_rust",
+    "myst_parser",
+    "sphinx_needs",
+    "sphinxcontrib.plantuml",
+    "sphinx.ext.intersphinx",
+]
 html_theme = "furo"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
@@ -12,3 +18,7 @@ rust_crates = [
     "../crates/analyzer",
     "../crates/py_binding",
 ]
+
+intersphinx_mapping = {
+    "sphinx": ("https://www.sphinx-doc.org", None),
+}
