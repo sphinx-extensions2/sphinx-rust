00bb3ae9ad236f5d5f66d03f1ad825d462da50ba
Chris Sewell <chrisj_sewell@hotmail.com>
2024-04-26 22:13:31 +0200

    Improve contributing documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ from sphinx_rust import __version__
 project = "sphinx-rust"
 version = __version__
 
-extensions = ["sphinx_rust", "sphinx_needs", "sphinxcontrib.plantuml"]
+extensions = ["sphinx_rust", "myst_parser", "sphinx_needs", "sphinxcontrib.plantuml"]
 html_theme = "furo"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
